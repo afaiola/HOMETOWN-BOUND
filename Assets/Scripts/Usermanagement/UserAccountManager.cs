a0a4bea3c1ba25e01e1bfb896a1e4dc7f8f3f196
Briken <brokenbriken@gmail.com>
2023-02-22 18:53:04 +0000

    Adding name and speech bubble
    Adding name handling and speech bubble in the intro scene
    Bug detail:
    First Name, Email, and password
    The user's first name is inserted into the dialogue when doctor releases patient;
    
    Evidence: https://drive.google.com/file/d/13xsmJITYoaKhZCR4GypDNTqvRkBVdlJk/view?usp=share_link

--- Assets/Scripts/Usermanagement/UserAccountManager.cs
+++ Assets/Scripts/Usermanagement/UserAccountManager.cs
@@ -53,7 +53,7 @@ public class UserAccountManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void StartSignIn(Dictionary<string, string> userOptions)
@@ -67,82 +67,70 @@ public class UserAccountManager : MonoBehaviour
     {
         // TODO: protect from retreiving invalid keys
         string email = userOptions[k_email];    // check if this is a username instead
-        string username = userOptions[k_user];   // retreive the username from firestore if not provided 
+        string username = userOptions[k_email];   // retreive the username from firestore if not provided 
         string password = userOptions[k_pass];
 
         var db = FirebaseFirestore.DefaultInstance;
         if (!email.Contains("@"))
         {
-            // Query firestore to get the email from the username
-            var usersRef = db.Collection(k_user_collection);
-            Query query = usersRef.WhereEqualTo(k_user, username);
-            var queryTask = query.GetSnapshotAsync();
-            yield return new WaitUntil(() => queryTask.IsCompleted);    
-
-            foreach (var doc in queryTask.Result.Documents)
-            {
-                Dictionary<string, object> user = doc.ToDictionary();
-                if (user.ContainsValue(username))
-                {
-                    email = doc.Id;
-                    username = (string)user[k_user];
-                    Debug.Log($"Username {username} -> {email}");
-                }
-            }
-        }
-
-        var auth = FirebaseAuth.DefaultInstance;
-        var task = auth.SignInWithEmailAndPasswordAsync(email, password);
-        yield return new WaitUntil(() => task.IsCompleted);
-
-        if (task.IsCanceled)
-        {
-            Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
-        }
-        if (task.IsFaulted)
-        {
-            Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
-        }
-
-        if (task.Exception == null)
-        {
-            var userRef = db.Collection(k_user_collection).Document(email);
-            var snapshotTask = userRef.GetSnapshotAsync();
-            yield return new WaitUntil(() => snapshotTask.IsCompleted);
-
-            var user = snapshotTask.Result.ToDictionary();
-
-            if (user != null)
-            {
-                string uname = "";
-                int ci = 0;
-                int skin = 0;
-                int login_ct = 0;
-                double playTime = 0;
-                DateTime startDate = DateTime.Today;
-                DateTime lastLogin = DateTime.Today;
-                int consecDays = 0;
-
-                if (user.ContainsKey(k_user)) uname = user[k_user].ToString();
-                if (user.ContainsKey(k_ci)) ci = int.Parse(user[k_ci].ToString());
-                if (user.ContainsKey(k_skin)) skin = int.Parse(user[k_skin].ToString());
-                if (user.ContainsKey(k_login_ct)) login_ct = int.Parse(user[k_login_ct].ToString());
-                if (user.ContainsKey(k_play_time)) playTime = double.Parse(user[k_play_time].ToString());
-                if (user.ContainsKey(k_start_date)) startDate = DateTime.Parse(user[k_start_date].ToString());
-                if (user.ContainsKey(k_last_login)) lastLogin = DateTime.Parse(user[k_last_login].ToString());
-                if (user.ContainsKey(k_days_played)) login_ct = int.Parse(user[k_days_played].ToString());
-
-                Profiler.Instance.UserSignedIn(uname, ci, skin, login_ct, playTime, startDate, lastLogin, consecDays);
-                signInPanel.gameObject.SetActive(false);
-                registerPanel.gameObject.SetActive(false);
-                loadingBar.SetActive(true);
-                loginSuccessEvent.Invoke();
-            }
+            // username no longer used for login purposes, fail login if email does not contain @
+            signInPanel.SubmitFail("Your Email must be a valid email address");
         }
         else
         {
-            signInPanel.SubmitFail(task.Exception.Message);
-            // need to find where the useful content is in the exception
+            var auth = FirebaseAuth.DefaultInstance;
+            var task = auth.SignInWithEmailAndPasswordAsync(email, password);
+            yield return new WaitUntil(() => task.IsCompleted);
+
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+            }
+
+            if (task.Exception == null)
+            {
+                var userRef = db.Collection(k_user_collection).Document(email);
+                var snapshotTask = userRef.GetSnapshotAsync();
+                yield return new WaitUntil(() => snapshotTask.IsCompleted);
+
+                var user = snapshotTask.Result.ToDictionary();
+
+                if (user != null)
+                {
+                    string uname = "";
+                    int ci = 0;
+                    int skin = 0;
+                    int login_ct = 0;
+                    double playTime = 0;
+                    DateTime startDate = DateTime.Today;
+                    DateTime lastLogin = DateTime.Today;
+                    int consecDays = 0;
+
+                    if (user.ContainsKey(k_user)) uname = user[k_user].ToString();
+                    if (user.ContainsKey(k_ci)) ci = int.Parse(user[k_ci].ToString());
+                    if (user.ContainsKey(k_skin)) skin = int.Parse(user[k_skin].ToString());
+                    if (user.ContainsKey(k_login_ct)) login_ct = int.Parse(user[k_login_ct].ToString());
+                    if (user.ContainsKey(k_play_time)) playTime = double.Parse(user[k_play_time].ToString());
+                    if (user.ContainsKey(k_start_date)) startDate = DateTime.Parse(user[k_start_date].ToString());
+                    if (user.ContainsKey(k_last_login)) lastLogin = DateTime.Parse(user[k_last_login].ToString());
+                    if (user.ContainsKey(k_days_played)) login_ct = int.Parse(user[k_days_played].ToString());
+
+                    Profiler.Instance.UserSignedIn(uname, ci, skin, login_ct, playTime, startDate, lastLogin, consecDays);
+                    signInPanel.gameObject.SetActive(false);
+                    registerPanel.gameObject.SetActive(false);
+                    loadingBar.SetActive(true);
+                    loginSuccessEvent.Invoke();
+                }
+            }
+            else
+            {
+                signInPanel.SubmitFail(task.Exception.Message);
+                // need to find where the useful content is in the exception
+            }
         }
     }
 
@@ -156,23 +144,23 @@ public class UserAccountManager : MonoBehaviour
         // TODO: protect from retreiving invalid keys
         string email = userOptions[k_email];
         string password = userOptions[k_pass];
-        string username = email;
+        string username = userOptions[k_user];
         int ci = 0;
         int skin = int.Parse(userOptions[k_skin]);
 
         string message = "";
         bool success = true;
         var db = FirebaseFirestore.DefaultInstance;
-        // Query for the new username already existing
+        // Query for the new email already existing
         var usersRef = db.Collection(k_user_collection);
-        Query query = usersRef.WhereEqualTo(k_user, username);
+        Query query = usersRef.WhereEqualTo(k_email, email);
         var queryTask = query.GetSnapshotAsync();
         yield return new WaitUntil(() => queryTask.IsCompleted);
 
         foreach (var doc in queryTask.Result.Documents)
         {
             Dictionary<string, object> user = doc.ToDictionary();
-            if (user.ContainsValue(username))
+            if (user.ContainsValue(email))
             {
                 message = $"email already in use!";
                 success = false;
@@ -200,7 +188,7 @@ public class UserAccountManager : MonoBehaviour
             {
                 //message = task.Exception.InnerException.Message;//message = task.Exception.Flatten().InnerExceptions[0].Message;
                 //message = task.Exception.Message;//message = task.Exception.Flatten().InnerExceptions[0].Message;
-                message = task.Exception.InnerExceptions[task.Exception.InnerExceptions.Count-1].Message;
+                message = task.Exception.InnerExceptions[task.Exception.InnerExceptions.Count - 1].Message;
             }
         }
 
---
+++ Assets/SpeechBubble.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechBubble : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StaticEvent.showSpeechBubble.AddListener(ActivateBubble);
+        StaticEvent.hideSpeechBubble.AddListener(DeactivateBubble);
+    }
+
+    void ActivateBubble(){
+        foreach(Transform child in transform){
+            if (child.gameObject.name == "Text"){
+                child.GetComponent<TMPro.TextMeshProUGUI>().text = child.GetComponent<TMPro.TextMeshProUGUI>().text.Replace("{name}", Profiler.Instance.currentUser.username);
+            }
+            child.gameObject.SetActive(true);
+        }
+    }
+    void DeactivateBubble(){
+        Destroy(this.gameObject);
+    }
+}
