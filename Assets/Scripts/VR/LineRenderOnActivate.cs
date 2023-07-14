3d9c7811ab1fb04598e56cb082d150dc7f809921
Michael Probst <mpprobst@gmail.com>
2023-07-14 09:18:35 -0400

    Create initial vr files

---
+++ Assets/Scripts/VR/LineRenderOnActivate.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LineRenderOnActivate : MonoBehaviour
+{
+    [SerializeField] private InputActionReference activateAction;
+
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private XRInteractorLineVisual lineVisual;
+
+    private void Start()
+    {
+        activateAction.action.Enable();
+        
+        activateAction.action.started += OnActivate;
+        //activateAction.action.performed += context => Debug.Log($"{context.action} performed");
+        activateAction.action.canceled += OnDeactivate;
+
+        OnDeactivate(new InputAction.CallbackContext());
+    }
+
+    private void Update()
+    {
+        //if (!activateAction.enabled) return;
+    }
+
+    public void OnActivate(InputAction.CallbackContext context)
+    {
+        lineRenderer.enabled = true;
+        lineVisual.enabled = true;
+    }
+
+    public void OnDeactivate(InputAction.CallbackContext context)
+    {
+        lineRenderer.enabled = false;
+        lineVisual.enabled = false;
+
+    }
+}
